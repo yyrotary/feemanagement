974115f2c040bfda2e3ca98e46c2c4aa4aa01509
unknown <chshin@ust.ac.kr>
2025-03-29 14:38:41 +0900

    fix:  API 명시

--- lib/notion.ts
+++ lib/notion.ts
@@ -1,7 +1,7 @@
 import { Client } from '@notionhq/client';
 
 export const notion = new Client({
-  auth: process.env.NOTION_API_KEY
+  auth: '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'
 });
 
 export const MEMBER_DB_ID = '1c47c9ec930b8057bbd9f8b6708a0294';
