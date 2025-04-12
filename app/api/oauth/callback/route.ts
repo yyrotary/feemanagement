0090327b226909df4780047b30a1388fd6456620
unknown <chshin@ust.ac.kr>
2025-04-12 16:15:21 +0900

    redirect uri 변경

--- app/api/oauth/callback/route.ts
+++ app/api/oauth/callback/route.ts
@@ -10,7 +10,7 @@ const getRedirectUri = () => {
   // 프로덕션 환경에서는 실제 도메인 사용
   if (isProduction) {
     console.log('production 환경입니다.');
-    return 'https://yyrotarybanking.vercel.app/api/oauth/callback';
+    return 'https://port-next-yyrotary-m9dtm49kf3785cc5.sel4.cloudtype.app/api/oauth/callback';
   }
   
   // 개발 환경에서는 localhost 사용
